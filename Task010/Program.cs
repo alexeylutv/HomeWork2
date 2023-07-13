d2e12cdd97e896732e29ed223e7e9ed88408d249
Alex <l3lik@list.ru>
2023-07-13 23:02:33 +0300

    final version of HomeWork

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number <100 || number >= 1000)
+{
+    Console.WriteLine("Число не трёхзначное, повторите ввод");
+    return;
+}
+
+else
+
+{
+    int secondN = number / 10 % 10;
+    Console.WriteLine("Вторая цифра числа: " + secondN);
+}
